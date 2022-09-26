9381660cdc2d5d433dfe88f6c85e09d2ed39fa55
perez--resseguier thybau <prt4545a@ens.fsi>
2022-09-26 19:11:43 +0200

    class Gaulois sans methode main

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,56 @@
+package personnages;
+
+public class Gaulois {
+	
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public int getForce() {
+		return force;
+	}
+	public void setForce(int force) {
+		this.force = force;
+	}
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+//	public static void main(String[] args) {
+//		if (condition) {
+//			
+//		} else {
+//
+//		}
+//		
+//		switch (key) {
+//		case value:
+//			
+//			break;
+//
+//		default:
+//			break;
+//		}
+//		
+//		for (int i = 0; i < args.length; i++) {
+//			
+//		}
+//		
+//		do {
+//			
+//		} while (condition);
+//		
+//		
+//	}
+}
