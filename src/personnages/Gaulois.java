c04410d2cc0da51d385e263f485f7fd7a2af0430
perez--resseguier thybau <prt4545a@ens.fsi>
2022-09-26 19:58:28 +0200

    class chef et village

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -7,9 +7,12 @@ public class Gaulois {
 	private int effetPotion = 1;
 	
 	public Gaulois(String nom, int force) {
-		super();
 		this.nom = nom;
 		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
 	}
 	
 	public int getForce() {
@@ -21,11 +24,28 @@ public class Gaulois {
 	public int getEffetPotion() {
 		return effetPotion;
 	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + ": ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
 
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
+	
+//	public static void main(String[] args) {
+////		TODO
+//	}
 	
 //	public static void main(String[] args) {
 //		if (condition) {
@@ -53,4 +73,5 @@ public class Gaulois {
 //		
 //		
 //	}
+	
 }
