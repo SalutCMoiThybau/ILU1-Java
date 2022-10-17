1388ce7d1609e2308cbcaf75ddd3647a4007582c
Thybau Pérez--Rességuier <thybaupr82@gmail.com>
2022-10-17 12:56:26 +0200

    test push pc region

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -24,7 +24,7 @@ public class Romain {
 	public void recevoirCoup(int forceCoup) {
 		force -= forceCoup;
 		if (force > 0) {
-			parler("Aïe");
+			parler("Aï¿½e !");
 		} else {
 			parler("J'abandonne...");
 		}
